e6abf8eaf2ee27a25f3ff39cb1e9a63e68c69204
Gleb Rasskazov <glebunium146@gmail.com>
2023-07-08 04:53:19 +0300

    commit with homework 5

--- ToDoApp/ToDoApp/Networking/URLSession.swift
+++ ToDoApp/ToDoApp/Networking/URLSession.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-//extension URLSession {
-//    func dataTask(for request: URLRequest) async throws -> (Data, URLResponse) {
-//        return try await withCheckedThrowingContinuation { continuation in
-//            let task = self.dataTask(with: request) { data, response, error in
-//                if let error = error {
-//                    continuation.resume(throwing: error)
-//                }
-//                if let data = data, let response = response {
-//                    continuation.resume(returning: (data, response))
-//                }
-//            }
-//            
-//            if Task.isCancelled {
-//                task.cancel()
-//            } else {
-//                task.resume()
-//            }
-//        }
-//    }
-//}
+// Homework 5
+extension URLSession {
+    func dataTask(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                }
+                if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                }
+            }
+            
+            if Task.isCancelled {
+                task.cancel()
+            } else {
+                task.resume()
+            }
+        }
+    }
+}
